30ca9ad66db7cb5ed9b8b15e5ba7cbc0a4d5beee
deepakmathpal <chiliyanaula>
2018-11-27 20:21:46 +0530

    eport

--- src/test/java/com/myPersonal/karate/CucumberReport.java
+++ src/test/java/com/myPersonal/karate/CucumberReport.java
@@ -1,4 +1,54 @@
 package com.myPersonal.karate;
 
-public class CucumberReport {
+import com.intuit.karate.cucumber.CucumberRunner;
+import com.intuit.karate.cucumber.KarateStats;
+import cucumber.api.CucumberOptions;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.joda.time.format.DateTimeFormat;
+import java.time.format.DateTimeFormatter;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Deepak
+ */
+@CucumberOptions(features = {"classpath:smoke", "classpath:Regression" },
+        tags = {"@Smoke","@Regression"}) // important: do not use @RunWith(Karate.class) !
+
+        public class CucumberReport {
+
+
+    @Test
+    public void testParallel() {
+        String karateOutputPath = "target/surefire-reports";
+        KarateStats stats = CucumberRunner.parallel(getClass(), 5, karateOutputPath);
+        generateReport(karateOutputPath);
+        assertTrue("there are scenario failures", stats.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+        LocalDateTime now = LocalDateTime.now();
+        String currentDateTime = dtf.format(now);
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        String currentDir = System.getProperty("user.dir");
+        String folderPath= currentDir + "\\Reports\\ " + "TestCaseExecution_ " +currentDateTime;
+        File file = new File (folderPath);
+        file.mkdirs();
+        Configuration config = new Configuration( file, "Cucumber_Reports");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+
 }
